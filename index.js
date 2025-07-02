587d9d0a5d6b96aae616d2eebc748dd6af4265e2
Laxy <laxy@example.com>
2025-07-02 06:44:02 +0300

    ilk commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
+
+client.once("ready", () => {
+  console.log(`🤖 Bot aktif: ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "!ping") {
+    message.reply("🏓 Pong!");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
