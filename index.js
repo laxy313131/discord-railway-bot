4abb2e760ff9cae0344eb29f1265f8f6ae5a5531
laxy313131 <iletisimlaxy@gmail.com>
2025-07-02 07:00:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits } = require('discord.js');
 
-// BOT_TOKEN'u doğrudan buraya yapıştır
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+// Tokeni buraya yapıştır
 const BOT_TOKEN = "NjkzNjQ4MTgxOTY3NDU0MzU1.GeFYFN.K6VXEu8pBvuV55TW0lvdTz-a0dt2zaI3G-nWvI";
 
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
-});
-
-client.once("ready", () => {
+client.once('ready', () => {
   console.log(`🤖 Bot aktif: ${client.user.tag}`);
 });
 
-client.on("messageCreate", (message) => {
+client.on('messageCreate', (message) => {
   if (message.content === "!ping") {
     message.reply("🏓 Pong!");
   }
